12060a139f40429bf0dc21cc9476d902961d2592
Brett Chapin <brett.chapin89@gmail.com>
2024-06-12 14:38:38 -0400

    Finished Notes API #5 - UPDATE and DELETE Requests

--- src/index.js
+++ src/index.js
@@ -9,6 +9,19 @@ const app = express();
 
 app.use(express.json())
 
+// CREATE
+app.post('/notes', async (req, res) => {
+    const note = new Note(req.body)
+
+    try {
+        await note.save()
+        res.status(200).send(note)
+    } catch (err) {
+        res.status(500).send(err)
+    }
+})
+
+// READ
 app.get('/notes', async (req, res) => {
     try {
         const notes = await Note.find({})
@@ -18,12 +31,34 @@ app.get('/notes', async (req, res) => {
     }
 })
 
-app.post('/notes', async (req, res) => {
-    const note = new Note(req.body)
-
+// UPDATE
+app.patch('/notes/:id', async (req, res) => {
     try {
+        const note = await Note.findById(req.params.id)
+        note.note = req.body.note
+
+        if (!note) {
+            return res.status(400).send("Note doesn't exist.")
+        }
+
         await note.save()
         res.status(200).send(note)
+    } catch (err) {
+        res.status(404).send(err)
+    }
+})
+
+// DELETE
+
+app.delete('/notes/:id', async (req, res) => {
+    try {
+        const note = await Note.findByIdAndDelete(req.params.id)
+
+        if (!note) {
+            return res.status(400).send("Note doesn't exist.")
+        }
+
+        res.status(200).send("The note has been deleted.")
     } catch (err) {
         res.status(500).send(err)
     }
